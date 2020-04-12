c1cbfa6fd37cab50466cb968cd5e96e64774b72a
Dávid Szakállas <david.szakallas@gmail.com>
2020-04-12 11:10:07 +0200

    use generic halve double generic control

--- packages/app/src/App/controls/loopMultiply.js
+++ packages/app/src/App/controls/loopMultiply.js
@@ -7,9 +7,8 @@ export default (gridPosition: [number, number]) => (deck: ChannelControl) => (_:
   const onMount = (k) => (dk: null, { bindings }: Object) => {
     bindings[k].button.sendColor(device.colors.lo_yellow)
   }
-  const onAttack = (k: 'double' | 'halve') => () => {
-    // TODO: remove unsafe cast once flow supports https://github.com/facebook/flow/issues/3637
-    deck[(`loop_${k}`: any)].setValue(1)
+  const onAttack = (amount: 0.5 | 2.0) => () => {
+    deck.loop_scale.setValue(amount)
   }
   return {
     bindings: {
@@ -17,13 +16,13 @@ export default (gridPosition: [number, number]) => (deck: ChannelControl) => (_:
         type: 'button',
         target: gridPosition,
         mount: onMount('halve'),
-        attack: onAttack('halve')
+        attack: onAttack(0.5)
       },
       double: {
         type: 'button',
         target: [gridPosition[0] + 1, gridPosition[1]],
         mount: onMount('double'),
-        attack: onAttack('double')
+        attack: onAttack(2.0)
       }
     }
   }
